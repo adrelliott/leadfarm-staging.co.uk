9ea2e04abca93262c72fc5de595cdf186b02044c
Al <user@als-macbook-pro.lan>
2013-06-08 12:28:13 +0200

    Updated production settings

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -15,7 +15,7 @@
 | path to your installation.
 |
 */
-$config['base_url'] = 'http://whosentmethis.co.uk/';
+$config['base_url'] = 'http://leadfarm-staging.co.uk/';
 
 /*
 |--------------------------------------------------------------------------
